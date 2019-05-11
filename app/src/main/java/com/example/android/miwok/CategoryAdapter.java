7a3dbf968adbb06c3815588934c9c6d4f74176f1
Mohamed Samy <muhamed.samy1@gmail.com>
2019-05-11 18:25:02 +0200

    Add tabs with the ViewPager

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,31 +1,58 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
-    public CategoryAdapter(FragmentManager fragmentManager) {
+    //Number of tabs
+    private final int PAGE_COUNT = 4;
+    // A Context object to call getResources().getString(int resID)
+    private Context mContext;
+
+    public CategoryAdapter(FragmentManager fragmentManager, Context context) {
         super(fragmentManager);
+        this.mContext = context;
     }
 
     @Override
     public Fragment getItem(int position) {
-        if (position == 0) {
-            return new NumbersFragment();
-        } else if (position == 1){
-            return new FamilyFragment();
-        }
-        else if (position == 2) {
-            return new ColorsFragment();
-        } else {
-            return new PhrasesFragment();
+        // Set Content for each page by selecting the relevant fragment
+        switch (position) {
+            case 0:
+                return new NumbersFragment();
+            case 1:
+                return new FamilyFragment();
+            case 2:
+                return new ColorsFragment();
+            case 3:
+                return new PhrasesFragment();
+            default:
+                return new NumbersFragment();
         }
     }
 
     @Override
     public int getCount() {
-        return 4;
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Set title for each page/tab
+        switch (position) {
+            case 0:
+                return "Numbers";
+            case 1:
+                return "Family";
+            case 2:
+                return "Colors";
+            case 3:
+                return "Phrases";
+            default:
+                return "Numbers";
+        }
     }
 }
